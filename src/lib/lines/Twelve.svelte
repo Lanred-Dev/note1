f676eb2fb022e95fcfe2c6bb0c2892bbeb05cf63
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-08-16 07:34:50 -0500

    Improvements.

--- src/lib/lines/Twelve.svelte
+++ src/lib/lines/Twelve.svelte
@@ -6,13 +6,19 @@
     let blur: number = 1;
 
     function onProgress(progress: number) {
-        blur = progress > 0.4 ? 102 - 100 * ((progress - 0.4) / 0.1) : 100;
+        blur = progress > 0.4 ? 101 - (100 * ((progress - 0.4) / 0.1)) : 100;
     }
 </script>
 
 <Line {offset} line={"Meeting every challenge undaunted and graced."}>
     <ParallaxLayer {offset} rate={0.6} {onProgress}>
-        <div style="--tw-blur: blur({blur}px);" class="absolute left-1/2 top-3/4 aspect-square w-[25%] -translate-x-1/2 blur-0">
+        <div style="--tw-blur: blur({blur}px); will-change: blur;" class="absolute left-1/2 top-3/4 aspect-square w-[25%] -translate-x-1/2 blur-0">
+            <svg class="h-full w-full fill-rose-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+            </svg>
+        </div>
+
+        <div class="absolute left-1/2 top-3/4 aspect-square w-[25%] -translate-x-1/2 blur-3xl">
             <svg class="h-full w-full fill-rose-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
             </svg>
