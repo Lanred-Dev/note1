70bc1101d305498d72879f3cb823c01fce89966f
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-08-16 00:02:20 -0500

    Performance improvements.

--- src/lib/lines/Seven.svelte
+++ src/lib/lines/Seven.svelte
@@ -17,11 +17,11 @@
 <Line {offset} line={"It is time for us to reclaim,"}>
     <ParallaxLayer {offset} rate={0.5} {onProgress}>
         <div class="relative top-[55%] aspect-square w-full overflow-hidden">
-            <div style="--tw-scale-x: {personScale}; --tw-scale-y: {personScale};  --tw-translate-x: {-personPosition - 50}%;" class="absolute left-1/2 top-1/2 aspect-square w-[7%] -translate-y-1/2 rounded-full bg-white" />
+            <div style="--tw-scale-x: {personScale}; --tw-scale-y: {personScale};  --tw-translate-x: {-personPosition - 50}%; will-change: transform, filter;" class="absolute left-1/2 top-1/2 aspect-square w-[7%] -translate-y-1/2 rounded-full bg-white" />
 
-            <div style="--tw-scale-x: {personScale}; --tw-scale-y: {personScale}; --tw-translate-x: {personPosition - 50}%;" class="absolute left-1/2 top-1/2 aspect-square w-[7%] -translate-y-1/2 rounded-full bg-white" />
+            <div style="--tw-scale-x: {personScale}; --tw-scale-y: {personScale}; --tw-translate-x: {personPosition - 50}%; will-change: transform, filter;" class="absolute left-1/2 top-1/2 aspect-square w-[7%] -translate-y-1/2 rounded-full bg-white" />
 
-            <div style="--tw-scale-x: {explosionScale}; --tw-scale-y: {explosionScale};" class="absolute left-1/2 top-1/2 aspect-square w-[50%] -translate-x-1/2 -translate-y-1/2 rounded-full bg-green-100 blur-xl" />
+            <div style="--tw-scale-x: {explosionScale}; --tw-scale-y: {explosionScale}; will-change: transform, filter;" class="absolute left-1/2 top-1/2 aspect-square w-[50%] -translate-x-1/2 -translate-y-1/2 rounded-full bg-green-100 blur-xl" />
         </div>
     </ParallaxLayer>
 </Line>
